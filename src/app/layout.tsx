88ade70993dac372c419b833420465198b282de3
darlingx1x <warhorr2@mail.ru>
2025-07-07 14:55:45 +0500

    test3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 'use client';
 import React from 'react';
 import './globals.css';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Header from './Header';
 import Footer from './Footer';
 
 const inter = Inter({ subsets: ['cyrillic', 'latin'] });
-
-export const metadata: Metadata = {
-  title: 'Финансовый мониторинг',
-  description: 'Учёт доходов и расходов из SMS через Telegram',
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
